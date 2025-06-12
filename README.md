6d2ce91cf2216e4fc76f7fc42aefd4296ba9636b
Ross Bulat <ross@jkrb.io>
2025-06-12 13:42:16 +0700

    add inquirer

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 OpenGov Summary CLI is a CLI tool using Polkassembly and OpenAI APIs to fetch proposal descriptions and provide a summary to users. After the initial proposal summary, the user is able to ask additional questions about the proposal, and the CLI will answer.
 
 
+## Packages Used
+
+### Linting:
+
+- [eslint](https://www.npmjs.com/package/eslint) (and plugins): For linting TypeScript code.
+- [prettier](https://www.npmjs.com/package/prettier): For code formatting.
+
+### CLI:
+
+- [commander](https://www.npmjs.com/package/commander): For building the CLI interface.
+- [inquirer](https://www.npmjs.com/package/inquirer): For interactive CLI prompts.
--- src/actions.ts
+++ src/actions.ts
@@ -1,7 +1,8 @@
 import type { OptionValues } from 'commander'
+import inquirer from 'inquirer'
 
 // Split string action - example implementation
-export const splitString = (str: string, options: OptionValues) => {
+export const splitString = async (str: string, options: OptionValues) => {
   const limit = options.first ? 1 : undefined
   const separator = options.separator
 
@@ -9,4 +10,14 @@ export const splitString = (str: string, options: OptionValues) => {
   console.log('separator: ', separator)
 
   console.log(str.split(options.separator, limit))
+
+  const { choice } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'Pick a fruit:',
+      choices: ['Apple', 'Banana', 'Cherry'],
+    },
+  ])
+  console.log(`You picked: ${choice}`)
 }
